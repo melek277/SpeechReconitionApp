6f52d5de2786d6cf2979ca5daf1da7a986b9a29c
melek277 <70953989+melek277@users.noreply.github.com>
2023-08-09 21:13:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ def main():
 
     r = sr.Recognizer()
 
-    # Wrap the app content in a container
-    with st.beta_container():
-        st.write("Click the 'Start Recording' button and speak something:")
-        if st.button("Start Recording"):
-            with sr.Microphone() as source:
-                st.write("Listening...")
-                audio = r.listen(source)
+    # Use CSS styling to adjust layout for mobile
+    st.markdown(
+        """
+        <style>
+        .stButton > button {
+            width: 100%;
+            text-align: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-                try:
-                    st.write("Processing...")
-                    text = r.recognize_google(audio)
-                    st.write("Transcription:", text)
-                except sr.UnknownValueError:
-                    st.write("Sorry, I could not understand audio.")
-                except sr.RequestError as e:
-                    st.write("Error:", e)
+    st.write("Click the 'Start Recording' button and speak something:")
+    if st.button("Start Recording"):
+        with sr.Microphone() as source:
+            st.write("Listening...")
+            audio = r.listen(source)
+
+            try:
+                st.write("Processing...")
+                text = r.recognize_google(audio)
+                st.write("Transcription:", text)
+            except sr.UnknownValueError:
+                st.write("Sorry, I could not understand audio.")
+            except sr.RequestError as e:
+                st.write("Error:", e)
 
 if __name__ == "__main__":
     main()
