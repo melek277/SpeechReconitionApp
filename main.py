13cb5343199b018fc78e9391963208f23cec3727
melek277 <70953989+melek277@users.noreply.github.com>
2023-08-09 21:09:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ def main():
 
     r = sr.Recognizer()
 
-    st.write("Click the 'Start Recording' button and speak something:")
-    if st.button("Start Recording"):
-        with sr.Microphone() as source:
-            st.write("Listening...")
-            audio = r.listen(source)
+    # Wrap the app content in a container
+    with st.beta_container():
+        st.write("Click the 'Start Recording' button and speak something:")
+        if st.button("Start Recording"):
+            with sr.Microphone() as source:
+                st.write("Listening...")
+                audio = r.listen(source)
 
-            try:
-                st.write("Processing...")
-                text = r.recognize_google(audio)
-                st.write("Transcription:", text)
-            except sr.UnknownValueError:
-                st.write("Sorry, I could not understand audio.")
-            except sr.RequestError as e:
-                st.write("Error:", e)
+                try:
+                    st.write("Processing...")
+                    text = r.recognize_google(audio)
+                    st.write("Transcription:", text)
+                except sr.UnknownValueError:
+                    st.write("Sorry, I could not understand audio.")
+                except sr.RequestError as e:
+                    st.write("Error:", e)
 
 if __name__ == "__main__":
     main()
